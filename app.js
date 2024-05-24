8f4c493dbc15b34950214f5adb0d6541d9cc458a
vmurali100 <vmurali100@users.noreply.github.com>
2024-05-24 18:49:53 +0530

    Fixed CORS

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-var app = express()
-app.use(cors())
+app.options('*', cors());
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
