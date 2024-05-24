87245e8f889a5e16b32c149578f4659e85d3e31d
vmurali100 <vmurali100@users.noreply.github.com>
2024-05-24 18:48:42 +0530

    Added CORS

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+var app = express()
+app.use(cors())
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
